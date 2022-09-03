b62cbc69658fbe64921b5283a0495c0629daa3a4
Julius <juliusatkinson@gmail.com>
2022-09-03 13:57:44 -0700

    injectMAnifest

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,49 @@
 import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig} from 'vite'
+import  { defineConfig } from 'vite'
+
+
 export default defineConfig({
   plugins: [
     VitePWA({ 
       registerType: 'autoUpdate',
       strategies: 'injectManifest',
+      injectManifest: {
+        "globPatterns": [
+            "**/*.html",
+          ],
+      },
       devOptions: {
         enabled: true
+
       }
     })
   ]
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { VitePWA } from 'vite-plugin-pwa'
+// import { defineConfig} from 'vite'
+// export default defineConfig({
+//   plugins: [
+//     VitePWA({ 
+//       registerType: 'autoUpdate',
+//       strategies: 'injectManifest',
+//       devOptions: {
+//         enabled: true
+//       }
+//     })
+//   ]
+// })
