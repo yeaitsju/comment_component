b064ed9d1120544dc48438e6cbe737061faf0c98
Julius <juliusatkinson@gmail.com>
2022-09-05 09:30:06 -0700

    fixed json

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,18 +2,22 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      strategies: "injectManifest",
-      injectManifest: {
-        globPatterns: ["**/*.html"],
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
+  // plugins: [
+  //   VitePWA({
+  //     registerType: "autoUpdate",
+  //     strategies: "injectManifest",
+  //     injectManifest: {
+  //       globPatterns: ["**/*.html"],
+  //     },
+  //     devOptions: {
+  //       enabled: true,
+  //     },
+  //   }),
+  // ],
+  build: {
+    outDir: "../dist",
+  },
+  base: "/",
 });
 
 // import { VitePWA } from 'vite-plugin-pwa'
